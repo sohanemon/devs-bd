024deb41011a6ad0dd24b9330e29c2b6be41c5d0
sohanemon <m.sohan.rse@gmail.com>
2022-07-28 10:04:28 +0600

    daisyui installation done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,18 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#000000",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
